5591020c242fcb98fe237a23048fa63b8adaae74
domingobishop <domingobishop@gmail.com>
2020-10-13 10:17:19 +0100

    aws proxy check

--- functions.php
+++ functions.php
@@ -1,21 +1,26 @@
 <?php 
 
 function tnatheme_globals() {
-	global $pre_path;
-	global $pre_crumbs;
-	if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
-		$pre_path = '';
-		$pre_crumbs = array(
-				'Get involved' => '/'
-			);
-	} else {
-		$pre_crumbs = array(
-				'About us' => '/about/',
-				'Get involved' => '/about/get-involved/'
-			);
-		$pre_path = '/about/get-involved';
-	}
+    global $pre_path;
+    global $pre_crumbs;
+    if (isset($_SERVER['HTTP_X_NGINX_PROXY'])) {
+        $pre_crumbs = array(
+            'About us' => '/about/',
+            'Get involved' => '/about/get-involved/'
+        );
+        $pre_path = '/about/get-involved';
+    } elseif (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Get involved' => '/'
+        );
+    } else {
+        $pre_crumbs = array(
+            'About us' => '/about/',
+            'Get involved' => '/about/get-involved/'
+        );
+        $pre_path = '/about/get-involved';
+    }
 }
 tnatheme_globals();
 
-?>
